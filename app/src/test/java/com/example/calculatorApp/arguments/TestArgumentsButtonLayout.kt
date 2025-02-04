7c0eebf7c4dabe54fb6de6841ed37383079b5f85
soulSalttrader <dusan.solarik@gmail.com>
2025-02-04 21:45:45 +0100

    test: add parameterized tests for ButtonLayoutRegular and ButtonLayoutWide properties
    - Introduced TestArgumentsButtonLayout class to provide common test data for both regular and wide button layouts.
    - Added test methods for verifying correct properties of ButtonLayoutRegular and ButtonLayoutWide in separate test classes.
    - Implemented parameterized tests to check properties such as weight, textAlign, alignment, modifier, fontSize, fontWeight, and shape for both layouts.

---
+++ app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsButtonLayout.kt
@@ -0,0 +1,42 @@
+package com.example.calculatorApp.arguments
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.calculatorApp.model.layout.button.ButtonLayoutRegular
+import com.example.calculatorApp.model.layout.button.ButtonLayoutWide
+import org.junit.jupiter.params.provider.Arguments
+import java.util.stream.Stream
+
+data class TestArgumentsButtonLayout(
+    val regular: ButtonLayoutRegular = ButtonLayoutRegular(),
+    val wide: ButtonLayoutWide = ButtonLayoutWide(),
+) : TestArguments {
+
+    fun provideRegularProperties(): Stream<Arguments> {
+       return Stream.of(
+            Arguments.of(regular.weight, 1f, "weight"),
+            Arguments.of(regular.textAlign, TextAlign.Center, "textAlign"),
+            Arguments.of(regular.alignment, Alignment.Center, "alignment"),
+            Arguments.of(regular.modifier, Modifier, "modifier"),
+            Arguments.of(regular.fontSize, 42.sp, "fontSize"),
+            Arguments.of(regular.fontWeight, FontWeight.Normal, "fontWeight"),
+            Arguments.of(regular.shape, CircleShape, "shape"),
+        )
+    }
+
+    fun provideWideProperties(): Stream<Arguments> {
+        return Stream.of(
+            Arguments.of(wide.weight, 2f, "weight"),
+            Arguments.of(wide.textAlign, TextAlign.Center, "textAlign"),
+            Arguments.of(wide.alignment, Alignment.Center, "alignment"),
+            Arguments.of(wide.modifier, Modifier, "modifier"),
+            Arguments.of(wide.fontSize, 42.sp, "fontSize"),
+            Arguments.of(wide.fontWeight, FontWeight.Normal, "fontWeight"),
+            Arguments.of(wide.shape, CircleShape, "shape"),
+        )
+    }
+}
---
+++ app/src/test/java/com/example/calculatorApp/model/layout/button/ButtonLayoutRegularTest.kt
@@ -0,0 +1,36 @@
+package com.example.calculatorApp.model.layout.button
+
+import com.example.calculatorApp.arguments.TestArgumentsButtonLayout
+import io.kotest.assertions.withClue
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class ButtonLayoutRegularTest {
+
+    @Nested
+    inner class ButtonLayoutRegularPropertiesTest {
+
+        // Arrange:
+        private fun provideArguments(): Stream<Arguments> {
+            return TestArgumentsButtonLayout().provideRegularProperties()
+        }
+
+        @ParameterizedTest
+        @MethodSource("provideArguments")
+        fun `should return the correct property for button layout`(
+            actualProperty: Any,
+            expectedProperty: Any,
+            propertyName: String,
+        ) {
+
+            // Act & Assert:
+            withClue("Failed for property: $propertyName.") {
+                actualProperty shouldBe expectedProperty
+            }
+        }
+    }
+}
