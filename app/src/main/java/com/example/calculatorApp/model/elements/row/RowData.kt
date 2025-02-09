7228efd4fed35c8fb9751ded0baedb8748f04821
soulSalttrader <dusan.solarik@gmail.com>
2025-02-09 15:57:10 +0100

    feat: add RowData and RowLayoutStandard classes for improved layout handling
    - Introduced RowData class to encapsulate row element data with associated layout.
    - Added RowLayoutStandard class to define default layout properties, including alignment, shape, weight, and spacing.
        - RowLayoutStandard implements ElementLayout, ElementLayoutPositioning, and HasColor for full layout control and color customization.
        - Enabled flexible styling and layout management for row elements.

---
+++ app/src/main/java/com/example/calculatorApp/model/elements/row/RowData.kt
@@ -0,0 +1,12 @@
+package com.example.calculatorApp.model.elements.row
+
+import com.example.calculatorApp.model.elements.ElementCategory
+import com.example.calculatorApp.model.elements.ElementCategoryStyleCollection
+import com.example.calculatorApp.model.elements.ElementColorStyle
+import com.example.calculatorApp.model.elements.ElementData
+import com.example.calculatorApp.model.layout.ElementLayout
+
+class RowData(
+    override val element: Row,
+    override val layout: ElementLayout,
+) : ElementData<ElementCategory<ElementColorStyle>, ElementCategoryStyleCollection<ElementColorStyle>, ElementLayout, ElementColorStyle>
---
+++ app/src/main/java/com/example/calculatorApp/model/layout/row/RowLayoutStandard.kt
@@ -0,0 +1,25 @@
+package com.example.calculatorApp.model.layout.row
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.example.calculatorApp.model.layout.ElementLayout
+import com.example.calculatorApp.model.layout.ElementLayoutPositioning
+import com.example.calculatorApp.model.layout.HasColor
+
+class RowLayoutStandard(
+    override val alignment: Alignment = Alignment.Center,
+    override val modifier: Modifier,
+    override val shape: Shape = RectangleShape,
+    override val weight: Float = 1f,
+
+    override val alignmentVertical: Alignment.Vertical = Alignment.CenterVertically,
+    override val arrangementHorizontal: Arrangement.Horizontal = Arrangement.spacedBy(8.dp),
+    override val arrangementVertical: Arrangement.Vertical = Arrangement.spacedBy(8.dp),
+
+    override val color: Color = Color.Green,
+) : ElementLayout, ElementLayoutPositioning, HasColor
