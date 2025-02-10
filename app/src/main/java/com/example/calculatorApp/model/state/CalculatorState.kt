009f4337295ad0b4c76821ae121db23a35e57757
soulSalttrader <dusan.solarik@gmail.com>
2025-02-10 12:12:18 +0100

    init: feat/calculator-state-engine

---
+++ app/src/main/java/com/example/calculatorApp/model/state/CalculatorState.kt
@@ -0,0 +1,11 @@
+package com.example.calculatorApp.model.state
+
+import com.example.calculatorApp.model.elements.button.Button
+import com.example.calculatorApp.model.symbols.SymbolButton
+
+data class CalculatorState(
+    val currentNumber: String = SymbolButton.ZERO.label,
+    val operation: Button? = null,
+    val previousNumber: String = "",
+    val activeButtonLabel: String = "",
+)
