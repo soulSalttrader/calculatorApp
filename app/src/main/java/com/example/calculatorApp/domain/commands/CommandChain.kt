31400ff1c633eab17d123c2b6de291d85467d7b2
soulSalttrader <dusan.solarik@gmail.com>
2025-03-04 13:34:06 +0100

    refactor: remove obsolete Command files
    - Removed Command files that are no longer needed after the previous refactoring.

--- app/src/main/java/com/example/calculatorApp/domain/commands/CommandChain.kt
+++
@@ -1,9 +0,0 @@
-package com.example.calculatorApp.domain.commands
-
-import com.example.calculatorApp.model.state.CalculatorState
-
-//class CommandChain(private vararg val commands: Command) : Command {
-//    override fun execute(state: CalculatorState): CalculatorState {
-//        return commands.fold(state) { currentState, command -> command.execute(currentState) }
-//    }
-//}
