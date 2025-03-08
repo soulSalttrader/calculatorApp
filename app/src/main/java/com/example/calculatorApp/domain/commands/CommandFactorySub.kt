a2f0ead72a49706fc673fb7608cb4a731c431112
soulSalttrader <dusan.solarik@gmail.com>
2025-03-08 20:19:51 +0100

    refactor: remove obsolete Command files
    - Removed CommandFactory, CommandHandler files that are no longer needed after the previous refactoring.

--- app/src/main/java/com/example/calculatorApp/domain/commands/CommandFactorySub.kt
+++
@@ -1,8 +0,0 @@
-package com.example.calculatorApp.domain.commands
-
-import com.example.calculatorApp.model.elements.button.Button
-
-//interface CommandFactorySub<T : Button> {
-//
-//    fun create(button: Button): Command
-//}
