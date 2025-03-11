12568cd946a2707fa8bddc41edec3c795e40af33
soulSalttrader <dusan.solarik@gmail.com>
2025-03-11 17:07:13 +0100

    feat: introduce basic AST nodes
    - Unary operations (e.g., Sign, Percentage) are immediately computed and replaced with their result before being added to the AST.
        - The AST consists only of Number and Binary nodes, as unary functions are handled at input time instead of during evaluation.
        - This simplifies the AST structure by storing only binary operations, reducing complexity.

--- app/src/main/java/com/example/calculatorApp/domain/ASTNode.kt
+++ app/src/main/java/com/example/calculatorApp/domain/ASTNode.kt
@@ -1,3 +1,7 @@
 package com.example.calculatorApp.domain
 
-sealed class ASTNode
+sealed class ASTNode {
+
+    data class Number(val value: Double) : ASTNode()
+    data class Binary(val operator: OperatorBinary, val left: ASTNode, val right: ASTNode) : ASTNode()
+}
