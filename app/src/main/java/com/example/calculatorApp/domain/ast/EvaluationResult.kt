0209b44aedd0fc1d28a2fab019f23a45d90df789
soulSalttrader <dusan.solarik@gmail.com>
2025-03-30 15:59:04 +0200

    feat: introduce EvaluationResult for unified numeric operations

---
+++ app/src/main/java/com/example/calculatorApp/domain/ast/EvaluationResult.kt
@@ -0,0 +1,43 @@
+package com.example.calculatorApp.domain.ast
+
+sealed interface EvaluationResult {
+    val value: Number
+
+    data class IntegerResult(override val value: Long) : EvaluationResult
+    data class DoubleResult(override val value: Double) : EvaluationResult
+
+    companion object {
+        fun normalizeResult(expression: Double) = when (expression % 1.0 == 0.0) {
+            true -> IntegerResult(expression.toLong())
+            else -> DoubleResult(expression)
+        }
+    }
+
+    operator fun plus(r: EvaluationResult): EvaluationResult {
+        val expression = this.value.toDouble() + r.value.toDouble()
+
+        return normalizeResult(expression)
+    }
+
+    operator fun minus(r: EvaluationResult): EvaluationResult {
+        val expression = this.value.toDouble() - r.value.toDouble()
+
+        return normalizeResult(expression)
+    }
+
+    operator fun times(r: EvaluationResult): EvaluationResult {
+        val expression = this.value.toDouble() * r.value.toDouble()
+
+        return normalizeResult(expression)
+    }
+
+    operator fun div(r: EvaluationResult): EvaluationResult {
+        val expression = this.value.toDouble().safeDiv(r.value.toDouble())
+
+        return normalizeResult(expression)
+    }
+
+    private fun Double.safeDiv(right: Double): Double {
+        return if (right != 0.0) this / right else Double.NaN
+    }
+}
