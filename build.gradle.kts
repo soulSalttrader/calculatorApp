913ba0ab7bbc3085ffc486dad7e5184be1bf4385
soulSalttrader <dusan.solarik@gmail.com>
2025-07-22 20:21:21 +0200

    chore: Add Parcelize plugin
    - Enables partial state persistence via SavedStateHandle across process death

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,6 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.compose.compiler) apply false
+
+    alias(libs.plugins.parcelize) apply false
 }
