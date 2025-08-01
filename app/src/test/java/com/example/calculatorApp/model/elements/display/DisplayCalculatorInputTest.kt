b3c5fd3d0a8d31e1eadb58a0cf62db502562e4b8
soulSalttrader <dusan.solarik@gmail.com>
2025-08-01 16:43:29 +0200

    refactor(test): update DisplayCalculatorInputTest to use Visuals and provideMappedTestData()

--- app/src/test/java/com/example/calculatorApp/model/elements/display/DisplayCalculatorInputTest.kt
+++ app/src/test/java/com/example/calculatorApp/model/elements/display/DisplayCalculatorInputTest.kt
@@ -1,37 +1,27 @@
 package com.example.calculatorApp.model.elements.display
 
-import TestArgumentsDisplay
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import com.example.calculatorApp.model.elements.ElementCategory
-import com.example.calculatorApp.model.elements.ElementCategoryStyleCollection
+import com.example.calculatorApp.arguments.TestArgumentsCalculatorElement.provideMappedTestData
 import com.example.calculatorApp.model.elements.ElementCategoryStyleCollectionImpl
 import com.example.calculatorApp.model.elements.ElementColorStyle
 import com.example.calculatorApp.model.styles.StylesDisplay
-import com.example.calculatorApp.utils.ColorToLongConverter
-import com.example.calculatorApp.utils.DisplayCalculatorList
+import com.example.calculatorApp.testData.TestDataCalculatorElement
+import com.example.calculatorApp.utils.DisplayCalculatorList.inputs
+import com.example.calculatorApp.utils.DisplayCalculatorMappings.inputsVisualsMap
+import com.example.calculatorApp.utils.VisualsDisplay
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.converter.ConvertWith
-import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
+import kotlin.streams.asStream
 
 class DisplayCalculatorInputTest {
 
     @Nested
     inner class GetCategory {
-
-        // Arrange: Setup test data (display instance)
-        private fun provideArguments(): Array<DisplayCalculatorInput> {
-            return DisplayCalculatorList.inputs
-        }
+        // Arrange:
+        private fun provideArguments(): Stream<DisplayCalculatorInput> = inputs.asStream()
 
         @ParameterizedTest
         @MethodSource("provideArguments")
@@ -45,13 +35,11 @@ class DisplayCalculatorInputTest {
         fun `should throw exception when category is not found`(display: DisplayCalculatorInput) {
             // Arrange: Create an empty style collection (default is empty)
             val emptyStyleCollection = ElementCategoryStyleCollectionImpl<ElementColorStyle>()
-
             // Act & Assert: Call getBackgroundColor, which internally calls getStyle and should throw an exception
             display.let {
                 val exception = shouldThrow<IllegalArgumentException> {
                     display.getBackgroundColor(emptyStyleCollection)
                 }
-
                 // Assert: Validate the exception message
                 exception.message shouldBe "Category '${display.getCategory()}' not found."
             }
@@ -61,49 +49,41 @@ class DisplayCalculatorInputTest {
     @Nested
     inner class GetBackgroundColor {
 
-        // Arrange: Setup test data (display instance)
-        private fun provideArguments(): Stream<Arguments> {
-            return TestArgumentsDisplay.provideInputsColors()
-        }
+        // Arrange:
+        private fun provideArguments(): Stream<TestDataCalculatorElement<DisplayCalculatorInput>> =
+            provideMappedTestData(inputs, inputsVisualsMap).asStream()
 
         @ParameterizedTest
         @MethodSource("provideArguments")
-        fun `should get the right backgroundColor for each input display`(
-            display: DisplayCalculatorInput,
-            @ConvertWith(ColorToLongConverter::class) expectedColor: Long,
+        fun `should get the right backgroundColor for each input display in iDisplayStyle`(
+            testData: TestDataCalculatorElement<DisplayCalculatorInput>
         ) {
             // Arrange:
             val style = StylesDisplay.iDisplayStyleInput
-
             // Act:
-            val actualColor = display.getBackgroundColor(style).toArgb()
-
+            val actualColor = testData.element.getBackgroundColor(style)
             // Assert:
-            actualColor shouldBe expectedColor
+            actualColor shouldBe (testData.expected as VisualsDisplay).background
         }
     }
 
     @Nested
-    inner class GetTextColor {
+    inner class GetForegroundColor {
 
-        private fun provideArguments(): Stream<Arguments> {
-            return TestArgumentsDisplay.provideInputsColors()
-        }
+        private fun provideArguments(): Stream<TestDataCalculatorElement<DisplayCalculatorInput>> =
+            provideMappedTestData(inputs, inputsVisualsMap).asStream()
 
         @ParameterizedTest
         @MethodSource("provideArguments")
-        fun `should get the right textColor for each input display`(
-            display: DisplayCalculatorInput,
-            @ConvertWith(ColorToLongConverter::class) expectedColor: Long,
+        fun `should get the right foreground color for each input display in iDisplayStyle`(
+            testData: TestDataCalculatorElement<DisplayCalculatorInput>
         ) {
             // Arrange:
             val style = StylesDisplay.iDisplayStyleInput
-
             // Act:
-            val actualColor = display.getBackgroundColor(style).toArgb()
-
+            val actualColor = testData.element.getForegroundColor(style)
             // Assert:
-            actualColor shouldBe expectedColor
+            actualColor shouldBe (testData.expected as VisualsDisplay).foreground
         }
     }
 }
