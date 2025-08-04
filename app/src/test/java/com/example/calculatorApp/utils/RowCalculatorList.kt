f59884796eb92b6ab56f99e4dc609b2daebbf9f6
soulSalttrader <dusan.solarik@gmail.com>
2025-08-04 18:39:38 +0200

    refactor(test): centralize test utilities from Button, Operator, and RowCalculatorList

--- app/src/test/java/com/example/calculatorApp/utils/RowCalculatorList.kt
+++ app/src/test/java/com/example/calculatorApp/utils/RowCalculatorList.kt
@@ -1,35 +1,21 @@
 package com.example.calculatorApp.utils
 
-import com.example.calculatorApp.model.elements.button.Button
 import com.example.calculatorApp.model.elements.button.ButtonData
-import com.example.calculatorApp.model.elements.row.Row
 import com.example.calculatorApp.model.elements.row.RowCalculatorStandard
 import com.example.calculatorApp.utils.ButtonCalculatorList.binary
 import com.example.calculatorApp.utils.ButtonCalculatorList.controls
 import com.example.calculatorApp.utils.ButtonCalculatorList.numbers
-import kotlin.reflect.KClass
-import kotlin.reflect.full.primaryConstructor
+import com.example.calculatorApp.utils.RowCalculatorMappings.standardVisualsMap
+import com.example.calculatorApp.utils.TestUtils.provideSequenceConstructed
 
 object RowCalculatorList {
+
     val buttonsBinaryTest = binary.map { ButtonData(it) }
     val buttonsControlsTest = controls.map { ButtonData(it) }
     val buttonsNumbersTest = numbers.map { ButtonData(it) }
 
-    private fun provideSequenceRows(rowClass: KClass<out Row>): Sequence<Row> {
-        return try {
-            rowClass.sealedSubclasses
-                .asSequence()
-                .mapIndexedNotNull { index, subclass ->
-                    val buttons = if (index % 2 == 0) buttonsBinaryTest else buttonsNumbersTest
-                    runCatching { subclass.constructors.first().call(buttons) }.getOrNull()
-                }
-        } catch (e: Exception) {
-            throw IllegalArgumentException("Unknown button class: $rowClass.")
-        }
-    }
-
-    val standards = provideSequenceRows(RowCalculatorStandard::class)
-
-//    val allRows: List<Row> = standards.toList()
-    val allRows: Sequence<Row> = standards
+    val standardRows = provideSequenceConstructed(
+        type = RowCalculatorStandard::class,
+        constructorArgs = standardVisualsMap,
+    )
 }
