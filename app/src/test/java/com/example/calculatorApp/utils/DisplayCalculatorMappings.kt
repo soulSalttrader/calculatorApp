2311ae5d137ebf2d4c8aa0487de00b2c4caa965f
soulSalttrader <dusan.solarik@gmail.com>
2025-08-04 20:58:19 +0200

    refactor(test): generalize test data type from DisplayCalculatorInput, DisplayCalculatorMappings to Display with Visuals

--- app/src/test/java/com/example/calculatorApp/utils/DisplayCalculatorMappings.kt
+++ app/src/test/java/com/example/calculatorApp/utils/DisplayCalculatorMappings.kt
@@ -1,19 +1,21 @@
 package com.example.calculatorApp.utils
 
 import com.example.calculatorApp.annotations.ConceptClass
+import com.example.calculatorApp.model.elements.display.Display
 import com.example.calculatorApp.model.elements.display.DisplayCalculatorInput
 import com.example.calculatorApp.ui.theme.Black
 import com.example.calculatorApp.ui.theme.White
+import kotlin.reflect.KClass
 
 object DisplayCalculatorMappings {
 
     @OptIn(ConceptClass::class)
-    val inputsVisualsMap: Map<DisplayCalculatorInput, VisualsDisplay> = mapOf(
-        DisplayCalculatorInput.Standard to VisualsDisplay(
+    val inputsVisualsMap: Map<KClass<out Display>, Visuals> = mapOf(
+        DisplayCalculatorInput.Standard::class to VisualsDisplay(
             background = Black,
             foreground = White,
         ),
-        DisplayCalculatorInput.Scientific to VisualsDisplay(
+        DisplayCalculatorInput.Scientific::class to VisualsDisplay(
             background = Black,
             foreground = White,
         ),
