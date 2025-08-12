ead1522edaf5a1999f9ef79a9f0a02b90e6202e6
soulSalttrader <dusan.solarik@gmail.com>
2025-08-12 20:57:41 +0200

    refactor(test): support multiple arguments in provideTokenizerTestCases
    - Chose vararg over alternatives such as Sequence<Sequence<HasSymbol>> or Iterable<Sequence<HasSymbol>>.
    - provideTokenizerTestCases now accepts multiple Sequence<HasSymbol> parameters.
    - Moved expectedMixed from TestArgumentsTokenizer to TestDataTokenizerSeq and converted it to a val.

--- app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsTokenizer.kt
+++ app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsTokenizer.kt
@@ -10,42 +10,27 @@ import com.example.calculatorApp.domain.ast.TokenizerUtils.toBinaryOperator
 import com.example.calculatorApp.domain.ast.TokenizerUtils.toParenthesisOperator
 import com.example.calculatorApp.domain.ast.TokenizerUtils.toUnaryOperator
 import com.example.calculatorApp.model.symbols.HasSymbol
-import com.example.calculatorApp.testData.input.Input
-import com.example.calculatorApp.testData.input.InputToken
 import com.example.calculatorApp.testData.TestCase
-import com.example.calculatorApp.testData.TestDataTokenizerSeq.tokensBinaryTest
-import com.example.calculatorApp.testData.TestDataTokenizerSeq.tokensNumberTest
-import com.example.calculatorApp.testData.TestDataTokenizerSeq.tokensParenthesisTest
-import com.example.calculatorApp.testData.TestDataTokenizerSeq.tokensUnaryPrefixTest
-import com.example.calculatorApp.testData.TestDataTokenizerSeq.tokensUnarySuffixTest
 import com.example.calculatorApp.testData.expected.Expected
 import com.example.calculatorApp.testData.expected.ExpectedToken
-import kotlin.collections.addAll
+import com.example.calculatorApp.testData.input.Input
+import com.example.calculatorApp.testData.input.InputToken
 
 object TestArgumentsTokenizer {
 
     fun provideTokenizerTestCases(
-        symbolSources: Sequence<HasSymbol>,
+        vararg symbolSources: Sequence<HasSymbol>,
     ): Sequence<TestCase<Input, Expected>> =
-        sequence {
-            symbolSources.forEach { source ->
+        symbolSources
+            .asSequence()
+            .flatten()
+            .map { source ->
                 val token = source.symbol.label.toSingleToken()
-                yield(
-                    TestCase(
-                        InputToken(listOf(source)),
-                        ExpectedToken(listOf(token))
-                    )
+                TestCase(
+                    InputToken(listOf(source)),
+                    ExpectedToken(listOf(token))
                 )
             }
-        }
-
-    fun expectedMixed(): List<Token> = buildList {
-        addAll(tokensNumberTest)
-        addAll(tokensBinaryTest)
-        addAll(tokensUnaryPrefixTest)
-        addAll(tokensUnarySuffixTest)
-        addAll(tokensParenthesisTest)
-    }
 
     private fun String.toSingleToken(): Token = when {
         isNumber() -> Token.Number(toDouble())
