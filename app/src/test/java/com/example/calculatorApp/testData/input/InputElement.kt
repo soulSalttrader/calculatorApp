34653cc6c42e61b602c5105862c60266ed53451e
soulSalttrader <dusan.solarik@gmail.com>
2025-09-03 21:43:52 +0200

    feat(test): add InputElement implementing Input
    - Introduce InputElement data class for Element test scenarios
    - Encapsulates input to simplify and standardize testing

---
+++ app/src/test/java/com/example/calculatorApp/testData/input/InputElement.kt
@@ -0,0 +1,15 @@
+package com.example.calculatorApp.testData.input
+
+import com.example.calculatorApp.model.elements.Element
+import com.example.calculatorApp.model.elements.ElementCategory
+import com.example.calculatorApp.model.elements.ElementCategoryStyleCollection
+import com.example.calculatorApp.model.elements.ElementColorStyle
+
+data class InputElement(
+    val element: Element
+        <
+            ElementCategory<ElementColorStyle>,
+            ElementCategoryStyleCollection<ElementColorStyle>,
+            ElementColorStyle
+        >
+) : Input
