24ab36d01b915a6118359fc9c9f79feced9408c0
soulSalttrader <dusan.solarik@gmail.com>
2025-09-03 22:03:16 +0200

    feat(test): add provideElementTestCases in TestArgumentsCalculatorElement (first draft)
    - Initial implementation for generating element test cases.

--- app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsCalculatorElement.kt
+++ app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsCalculatorElement.kt
@@ -1,6 +1,15 @@
 package com.example.calculatorApp.arguments
 
+import com.example.calculatorApp.model.elements.ElementCategoryStyleCollection
+import com.example.calculatorApp.model.elements.ElementColorStyle
+import com.example.calculatorApp.model.elements.button.Button
+import com.example.calculatorApp.model.elements.button.ButtonCalculatorUnary
+import com.example.calculatorApp.model.styles.StylesButton.iButtonStyle
 import com.example.calculatorApp.testData.TestCase
+import com.example.calculatorApp.testData.expected.Expected
+import com.example.calculatorApp.testData.expected.ExpectedElement
+import com.example.calculatorApp.testData.input.Input
+import com.example.calculatorApp.testData.input.InputElement
 import kotlin.reflect.KClass
 
 object TestArgumentsCalculatorElement : TestArguments {
@@ -19,4 +28,29 @@ object TestArgumentsCalculatorElement : TestArguments {
                 )
             }
         }
+
+    fun provideElementTestCases(
+        elements: Sequence<Button>,
+        style: ElementCategoryStyleCollection<ElementColorStyle> = iButtonStyle,
+        ): Sequence<TestCase<Input, Expected>> =
+            sequence {
+                elements.forEach { button ->
+                    val category = button.getCategory()
+                    val background = button.getBackgroundColor(style)
+                    val foreground = button.getForegroundColor(style)
+                    val isPrefix = button is ButtonCalculatorUnary.Sign
+                    yield(
+                        TestCase(
+                            InputElement(element = button),
+                            ExpectedElement.Button(
+                                background = background,
+                                foreground = foreground,
+                                category = category,
+                                symbol = button.symbol,
+                                isPrefix = isPrefix,
+                            )
+                        )
+                    )
+                }
+            }
 }
