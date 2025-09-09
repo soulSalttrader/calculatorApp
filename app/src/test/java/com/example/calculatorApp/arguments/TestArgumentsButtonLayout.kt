9b9d2abd23303e8ac3337b7effcb5d96b420cce6
soulSalttrader <dusan.solarik@gmail.com>
2025-09-09 21:43:15 +0200

    feat(test): decouple TestArgumentsButtonLayout implementation
    - Converted TestArgumentsButtonLayout from a data class (holding regular and wide button instances) to an object.
    - Removed provideRegularProperties and provideWideProperties in favor of a generic provideElementLayoutTestCases function.
    - Integrated with the TestCase framework for more flexible and reusable test case generation.

--- app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsButtonLayout.kt
+++ app/src/test/java/com/example/calculatorApp/arguments/TestArgumentsButtonLayout.kt
@@ -1,44 +1,26 @@
 package com.example.calculatorApp.arguments
 
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
-import com.example.calculatorApp.model.layout.button.ButtonLayoutRegular
-import com.example.calculatorApp.model.layout.button.ButtonLayoutWide
-import org.junit.jupiter.params.provider.Arguments
-import java.util.stream.Stream
+import com.example.calculatorApp.model.layout.ElementLayout
+import com.example.calculatorApp.testData.TestCase
+import com.example.calculatorApp.testData.expected.Expected
+import com.example.calculatorApp.testData.expected.ExpectedLayout
+import com.example.calculatorApp.testData.input.Input
+import com.example.calculatorApp.testData.input.InputLayout
 
-data class TestArgumentsButtonLayout(
-    val regular: ButtonLayoutRegular = ButtonLayoutRegular(),
-    val wide: ButtonLayoutWide = ButtonLayoutWide(),
-) : TestArguments {
+object TestArgumentsButtonLayout : TestArguments {
 
-    fun provideRegularProperties(): Stream<Arguments> {
-       return Stream.of(
-            Arguments.of(regular.alignment, Alignment.Center, "alignment"),
-            Arguments.of(regular.modifier, Modifier, "modifier"),
-            Arguments.of(regular.shape, CircleShape, "shape"),
-            Arguments.of(regular.weight, 1f, "weight"),
+    fun provideElementLayoutTestCases(
+        elementLayout: ElementLayout,
+        factoryInput: (ElementLayout) -> InputLayout<ElementLayout>,
+        factoryExpected: (ElementLayout) -> ExpectedLayout,
+    ): Sequence<TestCase<Input, Expected>> =
+        sequence {
 
-           Arguments.of(regular.alignText, TextAlign.Center, "textAlign"),
-            Arguments.of(regular.sizeFont, 42.sp, "fontSize"),
-            Arguments.of(regular.weightFont, FontWeight.Normal, "fontWeight"),
-        )
-    }
-
-    fun provideWideProperties(): Stream<Arguments> {
-        return Stream.of(
-            Arguments.of(wide.alignment, Alignment.Center, "alignment"),
-            Arguments.of(wide.modifier, Modifier, "modifier"),
-            Arguments.of(wide.shape, CircleShape, "shape"),
-            Arguments.of(wide.weight, 2f, "weight"),
-
-            Arguments.of(wide.alignText, TextAlign.Center, "textAlign"),
-            Arguments.of(wide.sizeFont, 42.sp, "fontSize"),
-            Arguments.of(wide.weightFont, FontWeight.Normal, "fontWeight"),
-        )
-    }
+            yield(
+                TestCase(
+                    input = factoryInput(elementLayout),
+                    expected = factoryExpected(elementLayout)
+                )
+            )
+        }
 }
