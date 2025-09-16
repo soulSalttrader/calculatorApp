bf28b5fcf48aed1c8f118b46c158ff9044062b75
soulSalttrader <dusan.solarik@gmail.com>
2025-09-16 21:25:20 +0200

    feat(test): add providers for percentage input and expected evaluation
    - Introduced engineMathPercentageInput to build InputEngineMath.Percentage from operand, previous number, and last operator.
    - Added engineMathPercentageExpected to generate ExpectedEngineMath.Percentage using evalPercentTest.

--- app/src/test/java/com/example/calculatorApp/testData/TestDataEngineMathStandardPercent.kt
+++ app/src/test/java/com/example/calculatorApp/testData/TestDataEngineMathStandardPercent.kt
@@ -1,6 +1,30 @@
 package com.example.calculatorApp.testData
 
+import com.example.calculatorApp.domain.ast.Operator
+import com.example.calculatorApp.testData.expected.ExpectedEngineMath
+import com.example.calculatorApp.testData.expected.ExpectedEngineMathResult
+import com.example.calculatorApp.testData.input.InputEngineMath
+import com.example.calculatorApp.testData.input.InputEval
+
 object TestDataEngineMathStandardPercent {
 
+    fun engineMathPercentageInput(
+        operand: Number,
+        previousNumber: Number?,
+        lastOperator: Operator?,
+    ): InputEngineMath.Percentage = InputEngineMath.Percentage(
+        object : InputEval.Percentage {
+            override val operand: Double = operand.toDouble()
+            override val lastOperator: Operator? = lastOperator
+            override val previousNumber: Double? = previousNumber?.toDouble()
+        }
+    )
 
+    fun engineMathPercentageExpected(
+        operand: Number,
+        previousNumber: Number?,
+        lastOperator: Operator?,
+    ): ExpectedEngineMath.Percentage = ExpectedEngineMath.Percentage(
+        ExpectedEngineMathResult.evalPercentTest(operand, previousNumber, lastOperator)
+    )
 }
