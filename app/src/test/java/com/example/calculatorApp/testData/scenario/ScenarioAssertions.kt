612a3c5e259160ad53f77342f965b6f803f61e10
soulSalttrader <dusan.solarik@gmail.com>
2025-09-21 23:16:37 +0200

    feat(test): rename ScenarioAssertions to ScenarioUtils and add state builder helpers
    - Renamed ScenarioAssertions file to ScenarioUtils for broader utility usage.
    - Added buildInputState and buildExpectedState inline helper functions to reduce duplication when creating test states.

--- app/src/test/java/com/example/calculatorApp/testData/scenario/ScenarioAssertions.kt
+++
@@ -1,4 +0,0 @@
-package com.example.calculatorApp.testData.scenario
-
-inline fun <reified T : Scenario> Scenario.requireScenario(): T =
-    this as? T ?: error("Requires ${T::class.simpleName} but got ${this::class.simpleName}")
