92117ba019e8e1ace1d4be37606c97d646e6d030
soulSalttrader <dusan.solarik@gmail.com>
2025-09-22 14:04:16 +0200

    refactor(test): use buildInputState/buildExpectedState in ScenarioEngineStateError and Update
    - Replaced custom engineStateBinaryX functions with buildInputState and buildExpectedState.
    - Integrated helpers into ScenarioEngineStateError and ScenarioEngineStateUpdate.
    - Removed redundant methods to reduce boilerplate and improve maintainability.

--- app/src/test/java/com/example/calculatorApp/testData/scenario/ScenarioEngineStateError.kt
+++ app/src/test/java/com/example/calculatorApp/testData/scenario/ScenarioEngineStateError.kt
@@ -3,17 +3,12 @@ package com.example.calculatorApp.testData.scenario
 import com.example.calculatorApp.domain.ast.Token
 import com.example.calculatorApp.domain.ast.TokenizerUtils.toOperator
 import com.example.calculatorApp.model.elements.button.Button
-import com.example.calculatorApp.testData.expected.ExpectedEngineState
-import com.example.calculatorApp.testData.expected.ExpectedEngineStateDelegate
-import com.example.calculatorApp.testData.input.InputEngineState
-import com.example.calculatorApp.testData.input.InputEngineStateDelegate
 import com.example.calculatorApp.testData.scenario.context.ContextEngineState
-import com.example.calculatorApp.testData.scenario.context.requireContext
 
 object ScenarioEngineStateError : ScenarioEngineState {
 
-    override val buildInput = ::engineStateBinaryInputError
-    override val buildExpected = ::engineStateBinaryExpectedError
+    override val buildInput = { context: ContextEngineState -> buildInputState<ContextEngineState.Error>(context) }
+    override val buildExpected = { context: ContextEngineState -> buildExpectedState<ContextEngineState.Error>(context) }
 
     override fun buildContexts(
         expressionInput: List<Token>,
@@ -30,18 +25,4 @@ object ScenarioEngineStateError : ScenarioEngineState {
         )
         return context to context
     }
-
-    fun engineStateBinaryInputError(context: ContextEngineState): InputEngineState.Binary =
-        InputEngineState.Binary(
-            object : InputEngineStateDelegate.Binary {
-                override val context: ContextEngineState.Base = context.requireContext<ContextEngineState.Error>()
-            }
-        )
-
-    fun engineStateBinaryExpectedError(context: ContextEngineState): ExpectedEngineState.Binary =
-        ExpectedEngineState.Binary(
-            object : ExpectedEngineStateDelegate.Binary {
-                override val context: ContextEngineState.Base = context.requireContext<ContextEngineState.Error>()
-            }
-        )
 }
--- app/src/test/java/com/example/calculatorApp/testData/scenario/ScenarioEngineStateUpdate.kt
+++ app/src/test/java/com/example/calculatorApp/testData/scenario/ScenarioEngineStateUpdate.kt
@@ -4,17 +4,12 @@ import com.example.calculatorApp.domain.ast.OperatorBinary
 import com.example.calculatorApp.domain.ast.Token
 import com.example.calculatorApp.domain.ast.TokenizerUtils.toOperator
 import com.example.calculatorApp.model.elements.button.Button
-import com.example.calculatorApp.testData.expected.ExpectedEngineState
-import com.example.calculatorApp.testData.expected.ExpectedEngineStateDelegate
-import com.example.calculatorApp.testData.input.InputEngineState
-import com.example.calculatorApp.testData.input.InputEngineStateDelegate
 import com.example.calculatorApp.testData.scenario.context.ContextEngineState
-import com.example.calculatorApp.testData.scenario.context.requireContext
 
 object ScenarioEngineStateUpdate : ScenarioEngineState {
 
-    override val buildInput = ::engineStateBinaryInputUpdate
-    override val buildExpected = ::engineStateBinaryExpectedUpdate
+    override val buildInput = { context: ContextEngineState -> buildInputState<ContextEngineState.Update>(context) }
+    override val buildExpected = { context: ContextEngineState -> buildExpectedState<ContextEngineState.Update>(context) }
 
     override fun buildContexts(
         expressionInput: List<Token>,
@@ -46,18 +41,4 @@ object ScenarioEngineStateUpdate : ScenarioEngineState {
         )
         return input to expected
     }
-
-    fun engineStateBinaryInputUpdate(context: ContextEngineState): InputEngineState.Binary =
-        InputEngineState.Binary(
-            object : InputEngineStateDelegate.Binary {
-                override val context: ContextEngineState.Base = context.requireContext<ContextEngineState.Update>()
-            }
-        )
-
-    fun engineStateBinaryExpectedUpdate(context: ContextEngineState): ExpectedEngineState.Binary =
-        ExpectedEngineState.Binary(
-            object : ExpectedEngineStateDelegate.Binary {
-                override val context: ContextEngineState.Base = context.requireContext<ContextEngineState.Update>()
-            }
-        )
 }
