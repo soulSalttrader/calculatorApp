2813e6fb72cdb34dcef652d5ae671e357b4f676d
soulSalttrader <dusan.solarik@gmail.com>
2025-09-23 20:42:21 +0200

    feat(test): add shouldMatch infix function and rename ContextAssertions to ContextUtils
    - Introduced shouldMatch infix function for concise field-by-field comparison between CalculatorStateDomain and ContextEngineState.Base in tests.
    - Renamed ContextAssertions file to ContextUtils to better reflect its purpose as a collection of testing utilities.

--- app/src/test/java/com/example/calculatorApp/testData/scenario/context/ContexAsseritons.kt
+++
@@ -1,4 +0,0 @@
-package com.example.calculatorApp.testData.scenario.context
-
-inline fun <reified T : Context> Context.requireContext(): T =
-    this as? T ?: error("Requires ${T::class.simpleName} but got ${this::class.simpleName}")
