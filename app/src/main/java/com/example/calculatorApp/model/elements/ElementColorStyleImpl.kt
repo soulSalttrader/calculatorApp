4eaa7491efe877f882c7dee1a5becd38ec88d2d7
soulSalttrader <dusan.solarik@gmail.com>
2025-10-06 10:53:43 +0200

    refactor: simplify ElementColorStyleImpl inheritance
    - Updated ElementColorStyleImpl to implement ElementColorStyle directly instead of extending ElementColorStyleBase.

--- app/src/main/java/com/example/calculatorApp/model/elements/ElementColorStyleImpl.kt
+++ app/src/main/java/com/example/calculatorApp/model/elements/ElementColorStyleImpl.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.graphics.Color
 class ElementColorStyleImpl(
     override val backgroundColor: Color,
     override val foregroundColor: Color,
-) : ElementColorStyleBase(backgroundColor, foregroundColor)
+) : ElementColorStyle
