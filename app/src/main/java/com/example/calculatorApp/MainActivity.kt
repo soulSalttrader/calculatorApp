f9538f3ab22ee3974a36264cf889741e9d02bb78
soulSalttrader <dusan.solarik@gmail.com>
2025-10-06 03:06:51 +0200

    feat: integrate CalculatorViewModel and Calculator composable into MainActivity
    - MainActivity now injects CalculatorViewModel via Hilt and renders the Calculator composable within CalculatorAppTheme.

--- app/src/main/java/com/example/calculatorApp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorApp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.calculatorApp.components.Calculator
+import com.example.calculatorApp.presentation.CalculatorViewModel
 import com.example.calculatorApp.ui.theme.CalculatorAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,7 +16,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CalculatorAppTheme { }
+            CalculatorAppTheme {
+                val viewModel: CalculatorViewModel = hiltViewModel()
+                viewModel.setOrientation()
+
+                Calculator(viewModel = viewModel)
+            }
         }
     }
 }
